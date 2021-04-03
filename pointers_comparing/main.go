d4575c2dcddb4a62e722a50194cc32d1b368276b
Filipe Coelho <coelho.sox@gmail.com>
2021-04-03 13:20:45 +0100

    pointers comparing

---
+++ pointers_comparing/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5.5
+	p1 := &a
+	pp1 := &p1
+	fmt.Printf("Value of p1: %v, address of p1: %v\n", p1, &p1)
+	fmt.Printf("Value of pp1: %v, address of pp1: %v\n", pp1, &pp1)
+
+	fmt.Printf("*p1 is %v\n", *p1)
+	fmt.Printf("*pp1 is %v\n", *pp1)
+}
